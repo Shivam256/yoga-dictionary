1fd24644cdafb215d87dd062b47e6285ab334338
Shivam256 <shivamgavandi256@gmail.com>
2021-03-20 19:19:33 +0530

    created the second page successfullygit add -A!

--- scripts/data.js
+++ scripts/data.js
@@ -1,6 +1,6 @@
 const data =[
   {
-    "id": 1,
+    "id":1,
     "sanskrit_name": "Navasana",
     "english_name": "Boat",
     "advantages": [
@@ -12,33 +12,33 @@ const data =[
       "Aids in stress relief",
       "Improves confidence"
     ],
-    "video": "https://youtu.be/8KsyQvwi85Q",
+    "video": "https://youtube.com/embed/8KsyQvwi85Q",
     "description": "The body comes into a V-shape, balancing entirely on the buttocks. In different variations and traditions, the arms, legs and torso may take different positions. In Paripurna Navasana, the legs and back are lifted high and arms extend forward and parallel to the ground. In Ardha Navasana, hands interlace behind the neck and both back and shoulders are closer to the ground.To come into the pose, begin seated on the floor. Bend your knees, bringing the soles of the feet to the ground, and bring the palms to the back of the thighs. Begin to lean back as you shift your weight from your feet, eventually lifting the soles of the feet off the ground. Balance on the sitting bones, not leaning right back on to the tailbone. Lengthen the spine to broaden and lift the chest.",
     "type": ["men", "athletes"]
   },
 
+  // {
+  //   "id":2,
+  //   "sanskrit_name": "Ardha Navasana",
+  //   "english_name": "Half-Boat",
+  //   "advantages": [
+  //     "Tones your muscles",
+  //     " Build strength in your body",
+  //     "Improves your balance",
+  //     "Proper digestion and immunity",
+  //     "Increases confidence and self worth",
+  //     "It helps in stimulating glands",
+  //     " Increases the digestion power",
+  //     "Regulates the menstrual cycle and hormonal imbalance."
+  //   ],
+  //   "video": "https://youtube.com/embed/CePu8V3vCvg",
+  //   "description": [
+  //     "Ardha navasana or half boat pose is a seated balancing posture that involves forward bend. Keeping the level of difficulty and amount of required strength in mind the pose lies in the intermediate level.Ardha asana is performed in the supine position. It targets working the abdominal muscles, adductor muscles, and hip flexors. Besides these, it also involves the quadriceps, hamstrings, and spine.Along with providing flexibility and strength to the body, it also strengthens and improves the mental health of the practitioner."
+  //   ],
+  //   "type": ["kids", "women"]
+  // },
   {
-    "id": 2,
-    "sanskrit_name": "Ardha Navasana",
-    "english_name": "Half-Boat",
-    "advantages": [
-      "Tones your muscles",
-      " Build strength in your body",
-      "Improves your balance",
-      "Proper digestion and immunity",
-      "Increases confidence and self worth",
-      "It helps in stimulating glands",
-      " Increases the digestion power",
-      "Regulates the menstrual cycle and hormonal imbalance."
-    ],
-    "video": "https://youtu.be/CePu8V3vCvg",
-    "description": [
-      "Ardha navasana or half boat pose is a seated balancing posture that involves forward bend. Keeping the level of difficulty and amount of required strength in mind the pose lies in the intermediate level.Ardha asana is performed in the supine position. It targets working the abdominal muscles, adductor muscles, and hip flexors. Besides these, it also involves the quadriceps, hamstrings, and spine.Along with providing flexibility and strength to the body, it also strengthens and improves the mental health of the practitioner."
-    ],
-    "type": ["kids", "women"]
-  },
-  {
-    "id": 3,
+    "id":3,
     "sanskrit_name": "Dhanurasana",
     "english_name": "Bow",
     "advantages": [
@@ -51,12 +51,12 @@ const data =[
       "Stimulate reproductive organs",
       "Improve the function of the pancreas."
     ],
-    "video": "https://youtu.be/4P2mYcOGxbU",
+    "video": "https://youtube.com/embed/4P2mYcOGxbU",
     "description": "This pose is so called because it looks like an archer’s bow, the torso and legs representing the body of the bow, and the arms the string.You can increase the challenge of Dhanurasana by performing the pose with your thighs, calves, and inner feet touching.",
     "type": ["athletes", "men", "women"]
   },
   {
-    "id": 4,
+    "id":4,
     "sanskrit_name": "Setu Bandha Sarvangasana",
     "english_name": "Bridge",
     "advantages": [
@@ -68,25 +68,26 @@ const data =[
       "Stimulates the lungs, thyroid glands, and abdominal organs.",
       "Improves digestion."
     ],
-    "video": "https://youtu.be/EZyBkVBUlG4",
+    "video": "https://youtube.com/embed/EZyBkVBUlG4",
     "description": "Setu Bandhasana is an asana in which the performer needs to assume the shape of a bridge. Therefore the asana is called as Setu Bandhasana or Bridge Pose. The asana helps in stretching your back, neck and chest while it relaxes your body as a whole. It also strengthens your legs, back and chest.",
     "type": ["men", "women", "kids", "athletes"]
-  },
+  }
+  // {
+  //   "id":5,
+  //   "sanskrit_name": "Baddha Konasana",
+  //   "english_name": "Butterfly",
+  //   "advantages": [
+  //     "Strengthens and improves flexibility in the inner thighs, groins and the knees.",
+  //     "Helps prepare the hips and groins for meditative seated poses, which require more flexibility in these areas.",
+  //     "Helps to sooth menstrual discomfort and digestive complaints."
+  //   ],
+  //   "video": "https://youtube.com/embed/hRcvSEtoecg",
+  //   "description": "Baddha Konasana (Sanskrit: बद्धकोणासन; IAST: baddhakoṇāsana), Bound Angle Pose, Throne Pose, Butterfly Pose, or Cobbler's Pose (after the typical sitting position of Indian cobblers when they work), and historically called Bhadrasana, is a seated asana in hatha yoga and modern yoga as exercise.",
+  //   "type": ["kids", "men"]
+  // }
+  ,
   {
-    "id": 5,
-    "sanskrit_name": "Baddha Konasana",
-    "english_name": "Butterfly",
-    "advantages": [
-      "Strengthens and improves flexibility in the inner thighs, groins and the knees.",
-      "Helps prepare the hips and groins for meditative seated poses, which require more flexibility in these areas.",
-      "Helps to sooth menstrual discomfort and digestive complaints."
-    ],
-    "video": "https://youtu.be/hRcvSEtoecg",
-    "description": "Baddha Konasana (Sanskrit: बद्धकोणासन; IAST: baddhakoṇāsana), Bound Angle Pose, Throne Pose, Butterfly Pose, or Cobbler's Pose (after the typical sitting position of Indian cobblers when they work), and historically called Bhadrasana, is a seated asana in hatha yoga and modern yoga as exercise.",
-    "type": ["kids", "men"]
-  },
-  {
-    "id": 6,
+    "id":6,
     "sanskrit_name": "Ustrasana",
     "english_name": "Camel",
     "advantages": [
@@ -99,12 +100,12 @@ const data =[
       "Loosens up the vertebrae.",
       "Relieves lower back pain."
     ],
-    "video": "https://youtu.be/8q7GxnIFsQo",
+    "video": "https://youtube.com/embed/8q7GxnIFsQo",
     "description": "Ustrasana is a deep backward bend from a kneeling position; the completed pose has the hands on the heels. The backs of the feet may be flat on the floor, or the toes may be tucked under for a slightly less strong backbend.",
     "type": ["women", "athletes"]
   },
   {
-    "id": 7,
+    "id":7,
     "sanskrit_name": "Marjaryasana",
     "english_name": "Cat",
     "advantages": [
@@ -116,12 +117,12 @@ const data =[
       "Creates emotional balance.",
       "Relieves stress and calms the mind."
     ],
-    "video": "https://youtu.be/Ov65nNSWrfw",
+    "video": "https://youtube.com/embed/Ov65nNSWrfw",
     "description": "Marjaryasana is an asana which needs the performer to assume the pose of a Marjari or Cat. Therefore this asana is called Marjaryasana or Cat Pose. This pose often includes a blend of another pose called Bitilasana (Cow Pose), which are similar poses with small variations",
     "type": ["women", "men"]
   },
   {
-    "id": 8,
+    "id":8,
     "sanskrit_name": "Bitilasana",
     "english_name": "Cow",
     "advantages": [
@@ -133,12 +134,12 @@ const data =[
       "It helps to stretch your neck and the front part of your torso."
     ],
 
-    "video": "https://youtu.be/Ov65nNSWrfw",
+    "video": "https://youtube.com/embed/Ov65nNSWrfw",
     "description": "A variant with the back lowered is Bitilasana (Sanskrit: बितिलासन; IAST: bitilāsana), Cow Pose; this is often used as the counter-posture, and a widely used exercise is to alternate between Cat and Cow Poses repeatedly.",
     "type": ["men", "athletes"]
   },
   {
-    "id": 9,
+    "id":9,
     "sanskrit_name": "Utkatasana",
     "english_name": "Chair",
     "advantages": [
@@ -148,13 +149,13 @@ const data =[
       "Helps open and create strength and stability in the shoulder joints.",
       "Tones abdomen and outer hips (gluteus medius and minimus)."
     ],
-    "video": "https://youtu.be/l654SqGBeOw",
+    "video": "https://youtube.com/embed/l654SqGBeOw",
     "description": "Utkatasana (Sanskrit: उत्कटासन; IAST: Utkaṭāsana), Chair Pose, is a standing asana in modern yoga as exercise. It was a low squatting asana in medieval hatha yoga.",
 
     "type": ["men", "kids", "women", "athletes"]
   },
   {
-    "id": 10,
+    "id":10,
     "sanskrit_name": "Balasana",
     "english_name": "Child's Pose",
     "advantages": [
@@ -162,7 +163,7 @@ const data =[
       "Calms the brain and helps relieve stress and fatigue.",
       "Relieves back and neck pain when done with head and torso supported."
     ],
-    "video": "https://youtu.be/2MJGg-dUKh0",
+    "video": "https://youtube.com/embed/2MJGg-dUKh0",
     "description": "Balasana is an easy yoga asana that can even be performed by beginners. In Sanskrit, bala means child and asana refers to one's posture. Thus, this pose is also called Child Pose. It is a 'counter' asana for many asanas and is performed preceding and following Sirsasana as it is a resting pose.",
     "type": ["men", "kids", "women", "athletes"]
   }
